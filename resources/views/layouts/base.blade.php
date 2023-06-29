d85e4c5c4cc290437e68e69ee2a44cb9d389a542
GianluGiarde <gianlugiarde@gmail.com>
2023-06-29 17:29:18 +0200

    add dark theme

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Dc Comics</title>
     @vite('resources/js/app.js')
 </head>
-<body>
+
+<body data-bs-theme="dark">
     <div class="container">
         @yield('content')
     </div>
 </body>
+
 </html>
