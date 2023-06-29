975909cf00710ded5b0ed373dfdc2df983db02b9
GianluGiarde <gianlugiarde@gmail.com>
2023-06-29 17:28:53 +0200

    add index & show

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.base')
+
+@section('content')
+    <h1>Tabella Fumetti</h1>
+    <hr>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Titolo</th>
+                <th scope="col">Prezzo (€)</th>
+                <th scope="col">Serie</th>
+                <th scope="col">Data di uscita</th>
+                <th scope="col">Tipologia</th>
+                <th scope="col">Azioni</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comics as $comic)
+                <tr>
+                    <td>{{ $comic->title }}</td>
+                    <td>{{ $comic->price / 100 }}</td>
+                    <td>{{ $comic->series }}</td>
+                    <td>{{ \Carbon\Carbon::parse($comic->date)->format('d/m/Y') }}</td>
+                    <td>{{ $comic->type }}</td>
+                    <td>
+                        <a class="btn btn-primary" href="{{ route('comics.show', ['comic' => $comic->id]) }}">Guarda</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.base')
+
+@section('content')
+    <div class="card">
+        <img src="{{ $comic->thumb }}" style="max-height: 1000px; object-fit: cover;" class="card-img-top"
+            alt="{{ $comic->title }}">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"><strong>TITOLO:</strong> {{ $comic->title }}</li>
+            <li class="list-group-item"><strong>DESCRIZIONE:</strong> {{ $comic->description }}</li>
+            <li class="list-group-item"><strong>SERIE:</strong> {{ $comic->series }}</li>
+            <li class="list-group-item"><strong>TIPOLOGIA:</strong> {{ $comic->type }}</li>
+            <li class="list-group-item"><strong>DATA DI USCITA:</strong> {{ $comic->sale_date }}</li>
+            <li class="list-group-item"><strong>PREZZO:(</strong>€) {{ $comic->price }}</li>
+            <li class="list-group-item"><strong>ARTISTI:</strong>
+                @foreach ($comic->artists as $artist)
+                    <span>{{ $artist }}, </span>
+                @endforeach
+            </li>
+            <li class="list-group-item"><strong>SCRITTORI:</strong>
+                @foreach ($comic->writers as $writer)
+                    <span>{{ $writer }}, </span>
+                @endforeach
+            </li>
+        </ul>
+    </div>
+@endsection
