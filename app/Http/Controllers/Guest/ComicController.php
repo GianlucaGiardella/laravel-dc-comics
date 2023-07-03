b7e93b745f4e2b406b2ee8d54f9ce3e37a813e84
GianluGiarde <gianlugiarde@gmail.com>
2023-07-03 15:01:50 +0200

    add validation

--- app/Http/Controllers/Guest/ComicController.php
+++ app/Http/Controllers/Guest/ComicController.php
@@ -8,11 +8,20 @@ use App\Http\Controllers\Controller;
 
 class ComicController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    private $validations = [
+        'title' => 'required|string|min:5|max:200',
+        'description' => 'string',
+        'thumb' => 'url|max:400',
+        'price' => 'required|integer|min:3',
+        'series' => 'required|string|min:5|max:80',
+        'sale_date' => 'required|date',
+        'type' => 'required|string|min:5|max:50',
+        'artists' => 'array',
+        'artists.*' => 'required|string',
+        'writers' => 'array',
+        'writers.*' => 'required|string',
+    ];
+
     public function index()
     {
         $comics = Comic::all();
@@ -20,35 +29,15 @@ class ComicController extends Controller
         return view("comics.index", compact("comics"));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view("comics.create");
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        $request->validate([
-            'title' => 'required|string|min:5|max:200',
-            'description' => 'string',
-            'thumb' => 'url|max:400',
-            'price' => 'required|integer|min:3',
-            'series' => 'required|string|min:5|max:80',
-            'sale_date' => 'required|date',
-            'type' => 'required|string|min:5|max:50',
-            'artists' => 'string',
-            'writers' => 'string',
-        ]);
+
+        $request->validate($this->validations);
 
         $data = $request->all();
 
@@ -67,46 +56,21 @@ class ComicController extends Controller
         return redirect()->route('comics.show', ['comic' => $newComic->id]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show(Comic $comic)
     {
         return view("comics.show", compact("comic"));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
