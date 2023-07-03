df91b3bf0fdcc26310d73aa260f1264129407350
GianluGiarde <gianlugiarde@gmail.com>
2023-07-03 17:54:45 +0200

    fix style

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -26,20 +26,21 @@
         </thead>
         <tbody>
             @foreach ($comics as $comic)
-                <tr>
+                <tr class="align-middle">
                     <td>{{ $comic->title }}</td>
                     <td>{{ $comic->price / 100 }}</td>
                     <td>{{ $comic->series }}</td>
                     <td>{{ \Carbon\Carbon::parse($comic->date)->format('d/m/Y') }}</td>
                     <td>{{ $comic->type }}</td>
                     <td>
-                        <a class="btn btn-primary" href="{{ route('comics.show', ['comic' => $comic->id]) }}">Guarda</a>
-                        <a class="btn btn-warning" href="{{ route('comics.edit', ['comic' => $comic->id]) }}">Modifica</a>
+                        <a class="btn btn-outline-info"
+                            href="{{ route('comics.show', ['comic' => $comic->id]) }}">&#8505;</a>
+                        <a class="btn btn-outline-light" href="{{ route('comics.edit', ['comic' => $comic->id]) }}">✏️</a>
                         <form action="{{ route('comics.destroy', ['comic' => $comic->id]) }}" method="POST"
                             class="d-inline-block">
                             @csrf
                             @method('delete')
-                            <button class="btn btn-secondary">Sposta nel Cestino</button>
+                            <button class="btn btn-outline-warning">&#128465;</button>
                         </form>
                     </td>
                 </tr>
